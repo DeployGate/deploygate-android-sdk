d74945b8b0df21d6bac4c1cedfc2b5287b03f5c0
Jumpei Matsuda <jmatsu.drm@gmail.com>
2020-09-11 11:51:27 +0900

    Fixed a typo in ci script

--- release.sh
+++ release.sh
@@ -7,6 +7,6 @@ set -euo pipefail
     sdk:bintrayUpload sdkMock:bintrayUpload \
     -PbintrayUser=$BINTRAY_USER \
     -PbintrayKey=$BINTRAY_KEY \
-    -PbintryDryRun=${BINTRAY_DRY_RUN:-true} \
-    -PbintryPublish=${BINTRAY_PUBLISH:-false} \
+    -PbintrayDryRun=${BINTRAY_DRY_RUN:-true} \
+    -PbintrayPublish=${BINTRAY_PUBLISH:-false} \
     --stacktrace
