091a284c41db325be2925ac2a6871cb2c1eb670e
Jumpei Matsuda <jmatsu.drm@gmail.com>
2022-04-09 01:35:45 +0900

    chore: format sdk module

--- sdk/src/main/java/com/deploygate/sdk/DeployGateProvider.java
+++ sdk/src/main/java/com/deploygate/sdk/DeployGateProvider.java
@@ -11,13 +11,20 @@ public class DeployGateProvider extends ContentProvider {
     @Override
     public boolean onCreate() {
         Context context = getContext();
-        if (context != null)
+        if (context != null) {
             DeployGate.install((Application) context.getApplicationContext(), null, true);
+        }
         return true;
     }
 
     @Override
-    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+    public Cursor query(
+            Uri uri,
+            String[] projection,
+            String selection,
+            String[] selectionArgs,
+            String sortOrder
+    ) {
         return null;
     }
 
@@ -27,17 +34,29 @@ public class DeployGateProvider extends ContentProvider {
     }
 
     @Override
-    public Uri insert(Uri uri, ContentValues values) {
+    public Uri insert(
+            Uri uri,
+            ContentValues values
+    ) {
         return null;
     }
 
     @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
+    public int delete(
+            Uri uri,
+            String selection,
+            String[] selectionArgs
+    ) {
         return 0;
     }
 
     @Override
-    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+    public int update(
+            Uri uri,
+            ContentValues values,
+            String selection,
+            String[] selectionArgs
+    ) {
         return 0;
     }
 }
