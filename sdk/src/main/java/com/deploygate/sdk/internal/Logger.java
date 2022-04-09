02d67378fc1051c37097eecc3c49be429200352f
Jumpei Matsuda <jmatsu.drm@gmail.com>
2022-04-09 18:24:34 +0900

    feat: add a logger and modified proguard rules

---
+++ sdk/src/main/java/com/deploygate/sdk/internal/Logger.java
@@ -0,0 +1,80 @@
+package com.deploygate.sdk.internal;
+
+import android.util.Log;
+
+import java.util.Locale;
+
+/**
+ * @hide
+ */
+public class Logger {
+    private static final String TAG = "DeployGateSDK";
+
+    public static void d(
+            String format,
+            Object... args
+    ) {
+        if (!Config.DEBUG) {
+            return;
+        }
+
+        Log.d(TAG, String.format(Locale.US, format, args));
+    }
+
+    public static void i(
+            String format,
+            Object... args
+    ) {
+        if (!Config.DEBUG) {
+            return;
+        }
+
+        Log.i(TAG, String.format(Locale.US, format, args));
+    }
+
+    public static void w(
+            String format,
+            Object... args
+    ) {
+        if (!Config.DEBUG) {
+            return;
+        }
+
+        Log.w(TAG, String.format(Locale.US, format, args));
+    }
+
+    public static void w(
+            Throwable th,
+            String format,
+            Object... args
+    ) {
+        if (!Config.DEBUG) {
+            return;
+        }
+
+        Log.w(TAG, String.format(Locale.US, format, args), th);
+    }
+
+    public static void e(
+            String format,
+            Object... args
+    ) {
+        if (!Config.DEBUG) {
+            return;
+        }
+
+        Log.e(TAG, String.format(Locale.US, format, args));
+    }
+
+    public static void e(
+            Throwable th,
+            String format,
+            Object... args
+    ) {
+        if (!Config.DEBUG) {
+            return;
+        }
+
+        Log.e(TAG, String.format(Locale.US, format, args), th);
+    }
+}
